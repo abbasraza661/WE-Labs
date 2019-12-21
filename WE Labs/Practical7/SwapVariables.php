d19331c16c927a929a2c8e8de51b4bda0bd63881
abbasraza661 <44173810+abbasraza661@users.noreply.github.com>
2019-12-21 14:11:44 +0500

    Add files via upload

---
+++ WE Labs/Practical7/SwapVariables.php
@@ -0,0 +1,14 @@
+<?php
+$a = 15;
+$b = 27;
+ 
+echo "\nThe number before swapping is:\n";
+echo "Number a =".$a." and b=".$b;
+
+$temp = $a;
+$a = $b;
+$b = $temp;
+ 
+echo "\nThe number after swapping is: \n";
+echo "Number a =".$a." and b=".$b."\n";
+?>
